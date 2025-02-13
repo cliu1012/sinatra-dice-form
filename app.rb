71ad52e0563c7222c924663dc8268d381ee06906
Claire Liu <cliu1012@gmail.com>
2025-02-13 17:05:31 +0000

    homepage

--- app.rb
+++ app.rb
@@ -2,6 +2,10 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
+   erb(:layout)
+end
+
+get("/process-roll") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
