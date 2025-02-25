567ddaa693f8c3a9b352c45213e0ca3149c51fb7
Claire Liu <cliu1012@gmail.com>
2025-02-25 22:00:19 +0000

    done

--- app.rb
+++ app.rb
@@ -2,12 +2,20 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-   erb(:layout)
+  erb(:index)
 end
 
-get("/process-roll") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/process_roll") do
+  # define variables
+  @dice = params.fetch("dice").to_i
+  @sides = params.fetch("sides").to_i
+  @rolls = Array.new
+
+  # roll the dice
+  @dice.times do
+    roll = rand(1..@sides)
+    @rolls.push(roll)
+  end
+
+  erb(:process_roll)
 end
